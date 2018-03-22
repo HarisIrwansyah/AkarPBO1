b3d1f888afe862aaab59c27404cc95643943c360
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2018-03-22 20:34:37 +0800

    Mengisi Nilai Atribut

--- src/akarpbo1/AkarTanamanAksi.java
+++ src/akarpbo1/AkarTanamanAksi.java
@@ -12,8 +12,28 @@ package akarpbo1;
 public class AkarTanamanAksi {
     public static void main(String[] args) {
         AkarTanaman Kacang = new AkarTanaman();
+            
+            Kacang.nama="Kacang Panjang";
+            Kacang.JenisAkar="Akar Serabut";
+            Kacang.PanjangAkar="5 - 10 Cm";
+            Kacang.JumlahAkar="Bercabang";
+            Kacang.KedalamanAkar="60 Cm";
+            
         AkarTanaman Singkong = new AkarTanaman();
+            
+            Singkong.nama="Singkong/Ubi Jalar";
+            Singkong.JenisAkar="Akar Serabut";
+            Singkong.PanjangAkar="30 Cm";
+            Singkong.JumlahAkar="Bercabang Banyak";
+            Singkong.KedalamanAkar="50 - 80 Cm";
+            
         AkarTanaman Mangga = new AkarTanaman();
+        
+            Mangga.nama="Pohon Mangga Madu";
+            Mangga.JenisAkar="Akar Tunggang";
+            Mangga.PanjangAkar="6 Meter";
+            Mangga.JumlahAkar="Bercabang Sedikit";
+            Mangga.KedalamanAkar="3 - 7 Meter";
     }
     
 }
